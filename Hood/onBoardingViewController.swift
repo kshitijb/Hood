751475f09edbcd9b3b5930e6dcb5755b4fef1c90
Robin Malhotra <me@rmalhotra.com>
2015-08-14 15:40:29 +0530

    the commit before the pull

--- Hood/onBoardingViewController.swift
+++ Hood/onBoardingViewController.swift
@@ -9,11 +9,41 @@
 import UIKit
 
 class onBoardingViewController: UIViewController {
-
+    let pages = 0...3
+    var views :[UIView] = []
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        scrollView.contentSize = CGSizeMake(view.frame.width*4, view.frame.height)
+        
+        for index in pages
+        {
+            let xOffset = CGFloat(index)*view.frame.width
+            let page = UIView(frame: CGRectMake(xOffset, 0, view.frame.width, view.frame.height))
+            var channelTitle = UILabel(frame: CGRectMake(xOffset, 35, view.frame.width, 37))
+            channelTitle.center = CGPointMake(view.center.x, channelTitle.center.y)
+            var channelDesc = UILabel(frame: CGRectMake(30, 85,  view.frame.width, 54))
+            channelTitle.numberOfLines = 2
+            channelTitle.center = CGPointMake(view.center.x, channelDesc.center.y)
+            
+            let iPhoneImage = UIImageView(image: UIImage(named: "iPhone"))
+            iPhoneImage.frame = CGRectMake(0,0,0,0)
+            var channelImageScrollView = UIScrollView(frame: CGRectMake(0,0,0,0))
+            page.addSubview(channelTitle)
+            page.addSubview(channelDesc)
+            page.addSubview(iPhoneImage)
+            page.addSubview(channelImageScrollView)
+            scrollView.addSubview(page)
+            switch index
+            {
+            case 1:
+                channelTitle.text = "#home"
+                channelDesc.text = "Chat with your neighbours. \\n Post news and get important alerts"
+                page.backgroundColor = 
+                
+            
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +52,5 @@ class onBoardingViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
